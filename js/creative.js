efd2460c751c5c68c65a70dbb8e694f022038086
drprog00 <hayk.adamyan@tumo.org>
2015-12-08 17:39:27 +0400

    Adding yandex analytics

--- js/creative.js
+++ js/creative.js
@@ -62,6 +62,8 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
+
+    //Google analytics
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -69,6 +71,9 @@
 
     ga('create', 'UA-70825202-1', 'auto');
     ga('send', 'pageview');
+
+    //Yandex analytics
+    (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter34084805 = new Ya.Metrika({ id:34084805, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");
 
 
 
