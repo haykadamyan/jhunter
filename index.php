5dff6de24cb6aca9042a48f81e6c57fcabf153f9
drprog00 <hayk.adamyan@tumo.org>
2015-12-26 14:40:42 +0400

    Fixed some bugs

--- index.php
+++ index.php
@@ -235,7 +235,7 @@
                         <label for="message">Ծրար։</label> <input type="checkbox" id="message" name="message">&nbsp
                         <label for="product">Այլ։</label> <input type="checkbox" id="product" name="product">
                     </div>
-                    <button type="submit" name="submit_question" class="btn btn-primary btn-xl wow tada" style="position: relative;top:10px;">Պատվիրեք առաքում հենց հիմա</button>
+                    <button type="submit" name="submit_question" class="btn btn-primary btn-xl" style="position: relative;top:10px;">Պատվիրեք առաքում հենց հիմա</button>
                 </form>
             </div>
         </div>
